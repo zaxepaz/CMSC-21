9699b276dd677592058f0a2e614de53d00acf3f2
zaxepaz <justinluistiu@gmail.com>
2022-03-10 11:30:00 +0800

    Lecture 2 Assignment

---
+++ Assignments/as1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+
+    int i,i2;
+
+    printf("Please enter a 2 digit number for reversing: ");
+    scanf("%1d%1d", &i,&i2);
+
+    printf("Reversed digit is %1d%1d", i2,i);
+
+}
